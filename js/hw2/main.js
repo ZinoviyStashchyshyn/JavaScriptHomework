9331bb29943213c5933c2e13e3e9ed14587fbee4
Zinoviy <zinoviy.stashchyshyn@gmail.com>
2022-07-27 11:05:47 +0300

    Arrays, objects

---
+++ js/hw2/main.js
@@ -0,0 +1,70 @@
+// // - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+//
+// let randomThings = [];                                       //// створення пустого масиву
+//
+// randomThings[0] = 1;                                        //// звертаючись до конкретного індексу масиву присвоюємо йому відповідні значення
+// randomThings[1] = 2;
+// randomThings[2] = 3.14;
+// randomThings[3] = "car";
+// randomThings[4] = "dog";
+// randomThings[5] = true;
+// randomThings[6] = false;
+// randomThings[7] = "London";
+// randomThings[8] = "Laguna";
+// randomThings[9] = 9;
+//
+// console.log(randomThings[0], randomThings[1], randomThings[2], randomThings[3], randomThings[4], randomThings[5],
+//             randomThings[6], randomThings[7], randomThings[8], randomThings[9]);
+//
+//
+// // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+//
+// let bookOne = {title: "Don Quixote", pageCount: 1077, genre: "novel"};
+//
+// let bookTwo = {tile: "Lord of the Rings", pageCount: 1178, genre: "high fantasy, adventure"};
+//
+// let bookThree = {title: "Harry Potter", pageCount: 1427, genre: "fantasy novel"};
+//
+//
+// // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+//
+// let bookOneDescription = {
+//     title: "Don Quixote",
+//     pageCount: 1077,
+//     genre: "novel",
+//     authors: ["Miguel de Cervantes", 1547 - 1616]
+// };
+//
+// let bookTwoDescription = {
+//     tile: "Lord of the Rings",
+//     pageCount: 1178,
+//     genre: "high fantasy, adventure",
+//     authors: ["J.R.R Tolkien", 1892 - 1973]
+// };
+//
+// let bookThreeDescription = {
+//     title: "Harry Potter",
+//     pageCount: 1427,
+//     genre: "fantasy novel",
+//     authors: ["J.K Rowling", 56]
+// };
+//
+//
+// //- Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+//
+// let users = [
+//     {name: "Oleg", username: "superman", password: "ttrdhdy3736438"},
+//     {name: "Igor", username: "uperman", password: "oytrdd65636438"},
+//     {name: "Sergiy", username: "super", password: "ddkkhdy3736438"},
+//     {name: "Vasya", username: "man", password: "dtrdhdy3799438"},
+//     {name: "Oleksandr", username: "perman", password: "d99lady3736438"},
+//     {name: "Svytoslav", username: "super", password: "dtr09493736438"},
+//     {name: "Taras", username: "uper", password: "ptrdhdy3736ksd"},
+//     {name: "Olga", username: "girl", password: "dtrdhdycnmcm38"},
+//     {name: "Katya", username: "supergirl", password: "ytrdhdy3736438"},
+//     {name: "Viktoria", username: "notbad", password: "dirdhdy3736438"}
+//
+// ];
+//
+// console.log(users[0].password, users[1].password, users[2].password, users[3].password, users[4].password,
+//             users[5].password, users[6].password, users[7].password, users[8].password, users[9].password);
